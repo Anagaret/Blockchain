1b4fa70a777ea2c9c06e7df19b4984530a7d6fdf
Anagaret <alexis.nagarettiname@gmail.com>
2021-05-04 12:09:57 +0200

    modif main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # tester notre code par le terminal 
 from classes.blockchain import Blockchain
 
-    blockchain = Blockchain('coucou', None)
-
-    print(blockchain)
+blockchain = Blockchain('coucou', None)
+print(blockchain)
