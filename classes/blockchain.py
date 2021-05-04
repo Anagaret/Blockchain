f1c55628703108eda7c22fb17004b98fcec9f246
nolan <nolan@e-major.fr>
2021-05-04 15:16:26 +0200

    function create hash "

--- classes/blockchain.py
+++ classes/blockchain.py
@@ -10,7 +10,8 @@ class Blockchain:
         self.list_blocks = []
         self.add_block(data)
 
-       
+    def create_hash(self, data):
+        return hashlib.sha256(str(data).encode('utf-8')).hexdigest()
 
     def add_block(self, data):
         if(len(self.list_blocks) == 0):
@@ -22,15 +23,17 @@ class Blockchain:
             index = previous_block.index + 1 
         timestamp = datetime.timestamp(datetime.now())
         nonce = 0
-        hash = hashlib.sha256(str({ 'index': index, 'timestamp': timestamp, 'nonce': nonce, 'previous_hash': previous_hash, 'data': data}).encode('utf-8')).hexdigest()
+        hash = self.create_hash(str({ 'index': index, 'timestamp': timestamp, 'nonce': nonce, 'previous_hash': previous_hash, 'data': data}))
         while not re.search(r"^[0]{4}", hash):
                 nonce +=1
-                hash = hashlib.sha256(str({ 'index': index, 'timestamp': timestamp, 'nonce': nonce, 'previous_hash': previous_hash, 'data': data}).encode('utf-8')).hexdigest()
+                hash = self.create_hash(str({ 'index': index, 'timestamp': timestamp, 'nonce': nonce, 'previous_hash': previous_hash, 'data': data}))
         self.list_blocks.append(Block(hash, data, previous_hash, nonce, index, timestamp))
 
     def validate_block(self, block):
-        hash = hashlib.sha256(str({ 'index': block.index, 'timestamp': block.timestamp, 'nonce': block.nonce, 'previous_hash': block.previous_hash, 'data': block.data}).encode('utf-8')).hexdigest()
+        hash = self.create_hash(str({ 'index': block.index, 'timestamp': block.timestamp, 'nonce': block.nonce, 'previous_hash': block.previous_hash, 'data': block.data}))
         return (hash == block.hash)
+
+   
 
 
 
